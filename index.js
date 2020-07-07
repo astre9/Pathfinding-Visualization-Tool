c63e1a78f362b70d5b5d22991e08696adb2f81cb
astre <Ctinnoicaaqw123>
2020-07-07 23:14:20 +0300

    UI changes & introduced Lottie Animations and clear board option

--- index.js
+++ index.js
@@ -1,78 +1,70 @@
 const nrRows = 31;
 const nrNodesPerRow = 70;
 const totalNodes = nrRows * nrNodesPerRow;
+const visitingTime = 10;
 
-const { nodesMatrix, visited } = initGrid();
+const algorithms = ["Djikstra", "BFS", "DFS"];
 
-window.onload = function() {
+window.onload = function () {
     feather.replace()
+    var selectedAlgorithm = -1;
 
-    var adjList = createAdjList(nodesMatrix);
+    initAnimations();
+
+    let algorithmItems = document.getElementsByClassName("item-algorithm");
+
+    for (let i = 0; i < algorithmItems.length; i++) {
+        algorithmItems[i].onclick = function (ev) {
+            selectedAlgorithm = i;
+            document.querySelector("#visualize").innerHTML = "Visualize " + algorithms[i];
+        };
+    }
+
+    const { nodesMatrix, visited } = initGrid();
 
     let btnVisualize = document.getElementById("visualize");
-    btnVisualize.onclick = async() => {
-        var predictedPath = [];
-        if (await BFSv2(adjList, predictedPath, nodesMatrix) == false) {
-            console.log("There is no path");
-        }
-        let path = [];
-        let start = 1110;
-        path.push(start);
-        while (predictedPath[start] != -1) {
-            path.push(predictedPath[start]);
-            start = predictedPath[start];
-        }
-        for (let i = path.length - 2; i > 0; i--) {
-            nodesMatrix[Math.floor(path[i] / nrNodesPerRow)][path[i] % nrNodesPerRow].classList.add("path-node");
-            await sleep(10);
+    btnVisualize.onclick = async () => {
+        if (selectedAlgorithm == 0) {
+
+        } else if (selectedAlgorithm == 1) {
+            visualizeBFS(nodesMatrix);
+        } else if (selectedAlgorithm == 2) {
+            visualizeDFS(nodesMatrix);
         }
     }
 
     let btnRandomPattern = document.getElementById("random-pattern");
-    btnRandomPattern.onclick = function() {
+    btnRandomPattern.onclick = function () {
         clearPattern(nodesMatrix, visited);
         generateRandomPattern(nodesMatrix, visited)
     };
 
+    let btnClear = document.getElementById("clear");
+    btnClear.onclick = function () {
+        clearAll(nodesMatrix, visited);
+    };
+
     let nodes = document.getElementsByClassName("node");
 
-    const createBlockedNode = function(node) {
-        if (!node.classList.contains("blocked-node")) {
-            node.classList.add("blocked-node")
-        } else {
-            node.classList.remove("blocked-node");
+    const createBlockedNode = function (node) {
+        if (!(node.classList.contains("start-node") || node.classList.contains("destination-node"))) {
+            if (!node.classList.contains("blocked-node")) {
+                node.classList.add("blocked-node")
+            } else {
+                node.classList.remove("blocked-node");
+            }
         }
     }
-    var mouseDown = false;
-    window.addEventListener("mousedown", function() {
+
+    window.addEventListener("mousedown", function () {
         for (let i = 0; i < nodes.length; i++) {
-            nodes[i].onmouseover = function(ev) {
+            nodes[i].onmouseover = function (ev) {
                 if (ev.buttons == 1) {
                     createBlockedNode(nodes[i])
                 }
             };
         }
     });
-
-    let drpAlgorithms = document.getElementById("drp-algorithms");
-    let iconAlgorithms = document.getElementById("icon-algorithms");
-    drpAlgorithms.onmouseenter = function() {
-        iconAlgorithms.removeAttribute("data-feather");
-        iconAlgorithms.setAttribute("data-feather", "chevron-up");
-        feather.replace();
-    }
-    drpAlgorithms.onmouseleave = function() {
-        iconAlgorithms.removeAttribute("data-feather");
-        iconAlgorithms.setAttribute("data-feather", "chevron-down");
-        feather.replace();
-    }
-    let btnDfs = document.getElementById("btn-dfs");
-    btnDfs.onclick = function() {
-        let adjList = createAdjList(nodesMatrix);
-        var stack = initStack();
-        DFS2(adjList);
-        // DFSIterative(adjList)
-    }
 
 }
 
@@ -102,16 +94,77 @@ function initGrid() {
     return { nodesMatrix: nodesMatrix, visited: visited };
 }
 
+function initAnimations() {
+    var animationTrash = bodymovin.loadAnimation({
+        container: document.getElementsByClassName('icon-trash')[0], // Required
+        path: './assets/trash-V2.json', // Required
+        renderer: 'svg', // Required
+        loop: false, // Optional
+        autoplay: false, // Optional
+        name: "trash-icon", // Name for future reference. Optional.
+    })
+    let btnClear = document.getElementById("clear");
+    btnClear.onmouseenter = function () {
+        animationTrash.setDirection(1);
+        animationTrash.play();
+    }
+    btnClear.onmouseleave = function () {
+        animationTrash.setDirection(-1);
+        animationTrash.play();
+    }
+    var animationSpeed = bodymovin.loadAnimation({
+        container: document.getElementById('icon-speed'), // Required
+        path: './assets/skip-forward.json', // Required
+        renderer: 'svg', // Required
+        loop: false, // Optional
+        autoplay: false, // Optional
+        name: "speed-icon", // Name for future reference. Optional.
+    })
+    let btnSpeed = document.getElementById("drp-speed");
+    btnSpeed.onmouseenter = function () {
+        animationSpeed.setDirection(1);
+        animationSpeed.play();
+    }
+    btnSpeed.onmouseleave = function () {
+        animationSpeed.setDirection(-1);
+        animationSpeed.play();
+    }
+}
+
 function visualize(nodesMatrix) {
     for (let i = 0; i < nrRows; i++) {
         for (let j = 0; j < nrNodesPerRow; j++) {
-            delayed(500, function(i, j) {
-                return function() {
+            delayed(500, function (i, j) {
+                return function () {
                     nodesMatrix[i][j].classList.add("visited-node");
                 };
             }(i, j));
         }
     }
+}
+
+async function visualizeBFS(nodesMatrix) {
+    let adjList = createAdjList(nodesMatrix)
+    var predictedPath = [];
+    if (await BFSv2(adjList, predictedPath, nodesMatrix) == false) {
+        console.log("There is no path");
+    }
+    let path = [];
+    let start = 1110;
+    path.push(start);
+    while (predictedPath[start] != -1) {
+        path.push(predictedPath[start]);
+        start = predictedPath[start];
+    }
+    for (let i = path.length - 2; i > 0; i--) {
+        nodesMatrix[Math.floor(path[i] / nrNodesPerRow)][path[i] % nrNodesPerRow].classList.add("path-node");
+        await sleep(visitingTime);
+    }
+}
+
+async function visualizeDFS(nodesMatrix) {
+    let adjList = createAdjList(nodesMatrix);
+    DFSIterative(adjList, nodesMatrix);
 }
 
 async function BFSv1(nodesMatrix) {
@@ -204,22 +257,25 @@ async function BFSv2(adjList, shortestPath, nodesMatrix, startNode = 1060, endNo
 }
 
 function initStack() {
-    var Stack = function() {
+    var Stack = function () {
         this.items = [];
     };
-    Stack.prototype.push = function(obj) {
+    Stack.prototype.push = function (obj) {
         this.items.push(obj);
     };
-    Stack.prototype.pop = function() {
+    Stack.prototype.pop = function () {
         return this.items.pop();
     };
-    Stack.prototype.isEmpty = function() {
+    Stack.prototype.peek = function () {
+        return this.items[this.items.length - 1];
+    };
+    Stack.prototype.isEmpty = function () {
         return this.items.length === 0;
     };
-    Stack.prototype.isExplored = function(n) {
+    Stack.prototype.isExplored = function (n) {
         return this.items.indexOf(n) !== -1;
     }
-    Stack.prototype.emptyOut = function() {
+    Stack.prototype.emptyOut = function () {
         this.items = [];
         return this.items.length === 0;
     };
@@ -227,57 +283,35 @@ function initStack() {
     return new Stack();
 }
 
-async function DFSIterative(adjList, startNode = 1060, endNode = 1110) {
-    var queue = [startNode];
+async function DFSIterative(adjList, nodesMatrix, startNode = 1060, endNode = 1110) {
     let visited = [];
+    let stack = initStack();
+    stack.push(startNode);
 
-    while (queue.length) {
-        startNode = queue.shift();
-        visited[startNode] = true;
-        nodesMatrix[Math.floor(startNode / nrNodesPerRow)][startNode % nrNodesPerRow].classList.add("visited-node");
-        await sleep(1);
+    while (stack.isEmpty() == false) {
 
-        if (startNode == endNode) {
+        let currentNode = stack.pop();
+
+        if (currentNode == endNode) {
             break;
         }
-        // if (stack.isExplored(startNode)) {
-        //     continue;
-        // }
-        stack.push(startNode);
 
-        if (visited != true) {
-            queue.push(adjList[startNode][0]);
-            visited[adjList[startNode][0]] = true;
+        if (currentNode != startNode) {
+            nodesMatrix[Math.floor(currentNode / nrNodesPerRow)][currentNode % nrNodesPerRow].classList.add("visited-node");
         }
-        previous = startNode;
-    }
+        await sleep(visitingTime);
 
-}
-
-async function DFS2(adjList, node = 1060, endNode = 1110) {
-    let s = initStack();
-    let explored = new Set();
-    s.push(node);
-
-    // Mark the first node as explored
-    explored.add(node);
-
-    // We'll continue till our Stack gets empty
-    while (!s.isEmpty() && s != endNode) {
-        let t = s.pop();
-
-        nodesMatrix[Math.floor(t / nrNodesPerRow)][t % nrNodesPerRow].classList.add("visited-node");
-        await sleep(1);
-
-        // 1. In the edges object, we search for nodes this node is directly connected to.
-        // 2. We filter out the nodes that have already been explored.
-        // 3. Then we mark each unexplored node as explored and push it to the Stack.
-        adjList[t]
-            .filter(n => !explored.has(n))
-            .forEach(n => {
-                explored.add(n);
-                s.push(n);
-            });
+        if (visited[currentNode] != true) {
+            visited[currentNode] = true;
+        }
+        for (let i = 0; i < adjList[currentNode].length; i++) {
+            // for (let i = adjList[currentNode].length - 1; i >= 0; i--) {
+            let nextNode = adjList[currentNode][i];
+            if (visited[nextNode] != true) {
+                stack.push(nextNode);
+            }
+        }
+        console.log(stack)
     }
 }
 
@@ -328,6 +362,7 @@ function createAdjList(nodesMatrix) {
     for (let i = 0; i < totalNodes; i++) {
         adjList[i] = [...new Set(adjList[i])];
     }
+
     return adjList;
 }
 
@@ -412,12 +447,29 @@ function clearPattern(nodesMatrix, visited) {
     }
 }
 
-var delayed = (function() {
+function clearAll(nodesMatrix, visited) {
+    for (let i = 0; i < nrRows; i++) {
+        for (let j = 0; j < nrNodesPerRow; j++) {
+            if (nodesMatrix[i][j].classList.contains("blocked-node")) {
+                nodesMatrix[i][j].classList.remove("blocked-node");
+            }
+            if (nodesMatrix[i][j].classList.contains("visited-node")) {
+                nodesMatrix[i][j].classList.remove("visited-node");
+            }
+            if (nodesMatrix[i][j].classList.contains("path-node")) {
+                nodesMatrix[i][j].classList.remove("path-node");
+            }
+            visited[i][j] = false;
+        }
+    }
+}
+
+var delayed = (function () {
     var queue = [];
 
     function processQueue() {
         if (queue.length > 0) {
-            setTimeout(function() {
+            setTimeout(function () {
                 queue.shift().cb();
                 processQueue();
             }, queue[0].delay);
